6123d275dd0b0b2aa3c1bc4e1869a9bc831781bb
Trevor <trezrevner@icloud.com>
2025-11-05 18:53:10 -0500

    Corrected row and column issue

--- program_3/wernerTpgm3.c
+++ program_3/wernerTpgm3.c
@@ -21,18 +21,16 @@ I certify that this work is my own alone.
 #include <stdio.h>
 #include <stdlib.h>
 
-// Constants
-#define MIN 1
-#define MAX 1000
-
 /**
 * 2b) Load each index w/ the multiplication of its x, y
 */
 
 void populateArray(int arr[10][10]) 
 {
-    for (int x = 0; x < 10; x++) {
-        for (int y = 0; y < 10; y++) {
+    int x;
+    for (x = 0; x < 10; x++) {
+        int y;
+        for (y = 0; y < 10; y++) {
             arr[x][y] = x * y;
         }
     }
@@ -42,15 +40,17 @@ void populateArray(int arr[10][10])
 // 2d) Print the total of columns 2, 4 and 6
 void printColumnTotals(int arr[][10]) 
 {
-    int col2 = 0, col3 = 0, col4 = 0, col5 = 0, col6 = 0, col7 = 0;
+    int row2 = 0, col3 = 0, row4 = 0, col5 = 0, row6 = 0, col7 = 0;
 
-    for (int row = 0; row < 10; row++) {
-        col2 += arr[row][1];
-        col3 += arr[row][2];
-        col4 += arr[row][3];
-        col5 += arr[row][4];
-        col6 += arr[row][5];
-        col7 += arr[row][6];
+    int idx;
+    // Iterates and alternates rows and columns
+    for (idx = 0; idx < 10; idx++) {
+        row2 += arr[1][idx];
+        col3 += arr[idx][2];
+        row4 += arr[3][idx];
+        col5 += arr[idx][4];
+        row6 += arr[5][idx];
+        col7 += arr[idx][6];
     }
 
     printf("Columns 3, 5, 7 totals ------\n");
@@ -58,52 +58,89 @@ void printColumnTotals(int arr[][10])
     printf("Column 5 Total: %d\n", col5);
     printf("Column 7 Total: %d\n", col7);
 
-    printf("Columns 2, 4, 6 totals ------\n");
-    printf("Column 2 Total: %d\n", col2);
-    printf("Column 4 Total: %d\n", col4);
-    printf("Column 6 Total: %d\n", col6);
+    printf("Rows 2, 4, 6 totals ------\n");
+    printf("Row 2 Total: %d\n", row2);
+    printf("Row 4 Total: %d\n", row4);
+    printf("Row 6 Total: %d\n", row6);
+    printf("----------------------------\n");
+}
+
+// Prints Rows 2,4,6 total - Columns 3, 5, 7 total
+void printSpecificDifferences(int arr[][10])
+{
+    int rowTotals = 0, colTotals = 0;
+
+    int row;
+    // Tally rows 2, 4, 6
+    for (row = 2; row < 7; row += 2) {
+        int col;
+        for (col = 0; col < 10; col++) {
+            rowTotals += arr[row][col];
+        }
+    }
+  
+    // Tally cols 3, 5, 7
+    for (row = 0; row < 10; row++) {
+        int col;
+        for (col = 3; col < 8; col += 2) {
+            colTotals += arr[row][col];
+        }
+    }
+
+    printf("Rows 2,4,6 - Columns 3,5,7 totals = %d\n", rowTotals - colTotals);
 }
 
 // 2e) Print the difference of the total values (rows - columns)
-void printDifferences(int arr[][10]) 
+void printTotalDifferences(int arr[][10]) 
 {
     int rowTotals = 0, colTotals = 0;
 
     // Tally each row  
-    for (int x = 0; x < 10; x++) {
-        int rowTotal = 0;
-        for (int y = 0; y < 10; y++) {
+    int x, rowTotal;
+    for (x = 0; x < 10; x++) {
+        rowTotal = 0;
+        int y;
+        for (y = 0; y < 10; y++) {
             rowTotal += arr[x][y];
         }
         rowTotals += rowTotal;
         printf("Row %d total: %d\n", x, rowTotal);
     }
+    printf("---------------------------\n");
 
     // Tally each column
-    for (int y = 0; y < 10; y++) {
+    int y;
+    for (y = 0; y < 10; y++) {
         int colTotal = 0;
-        for (int x = 0; x < 10; x++) {
+        int x;
+        for (x = 0; x < 10; x++) {
             colTotal += arr[x][y];
         }
         colTotals += colTotal;
         printf("Column %d total: %d\n", y, colTotal);
     }
+    printf("-------------------------\n");
 
     printf("Row totals: %d\n", rowTotals);
     printf("Column totals: %d\n", colTotals);
+    // Print Rows 2,4,6 totals - columns 3,5,7 differences
+    printSpecificDifferences(arr);
     // Print total difference b/n all rows and all columns
     printf("Rows - Columns = %d\n", rowTotals - colTotals);
+    printf("-------------------------\n");
 }
 
 // Random number generation between MIN and MAX
 int generateRandomNumber() 
 {
+    int MIN = 1, MAX = 1000; 
     return rand() % (MAX - MIN + 1) + MIN;
 }
 
 // Use swaps to sort three numbers into ascending order
 void ascending(int num1, int num2, int num3) 
 {
+    printf("Swapping three values into sorted position...\n");
     printf("Data Received: %5d %5d %5d\n", num1, num2, num3);
     int temp;
 
@@ -119,7 +156,14 @@ void ascending(int num1, int num2, int num3)
         num3 = temp;
     } 
 
+    if (num1 > num3) {
+        temp = num1;
+        num1 = num3;
+        num3 = temp;
+    }
+
     printf("Processed Data: %5d %5d %5d\n", num1, num2, num3);
+    printf("--------------------------\n");
 }
 
 // 4.) Print uppercase alphabet from Z to A using ASCII values
@@ -128,7 +172,7 @@ void printAlphabetReverse()
     int upperCaseZCode = 'Z', upperCaseACode = 'A';
 
     while(1) {
-        printf("%c > ASCII value = %i\n", upperCaseZCode, upperCaseZCode);
+        printf("%c > ASCII value = %d\n", upperCaseZCode, upperCaseZCode);
         upperCaseZCode--;
         if (upperCaseZCode < upperCaseACode) {
             break;
@@ -143,20 +187,20 @@ void printDivisionByZero()
     const int exitCode = 999;
     int firstNumber, secondNumber;
 
-    printf("##### Divider Program #######\n");
+    printf("\n##### Divider Program #######\n");
     printf("Enter '999' to quit at any time\n");
 
     while (1) {
         // Ask for user input of 1st and 2nd number
         printf("Enter the first number: ");
-        scanf("%i", &firstNumber);
+        scanf("%d", &firstNumber);
         // Check for exit code
         if (firstNumber == exitCode) {
             return;
         }
 
         printf("Enter a second number: ");
-        scanf("%i", &secondNumber);
+        scanf("%d", &secondNumber);
 
         // Check for exit code
         if (secondNumber == exitCode) {
@@ -167,7 +211,7 @@ void printDivisionByZero()
         while (secondNumber == 0) {
             // Prompt for re-entry of second number
             printf("Please re-enter the second number (cannot be zero): ");
-            scanf("%i", &secondNumber);
+            scanf("%d", &secondNumber);
 
             if (secondNumber == exitCode) {
                 return;
@@ -176,7 +220,8 @@ void printDivisionByZero()
         // Divide both numbers along with the division result
         float divisionResult = (float) firstNumber / secondNumber;
 
-        printf("%i divided by %i = %.2f\n", firstNumber, secondNumber, divisionResult);
+        printf("%d divided by %d = %.2f\n", firstNumber, secondNumber, divisionResult);
+        printf("-----------------------\n");
     }
 }
 
@@ -189,7 +234,7 @@ void printMod5Equal3()
     while (start <= 100) {
         res = start % modulo;
         if (res == 3) {
-            printf("%i mod %i = %i\n", start, modulo, res);
+            printf("%d mod %d = %d\n", start, modulo, res);
         }
         start++;
     }
@@ -199,12 +244,12 @@ void printMod5Equal3()
 // 7.) Print results of two single-dimensional arrays
 void printTwo1DArrays() 
 {
-    char one[3][50] = {"This ANSI C ", "at ", "is "};
-    char two[3][50] = {"class ", "FIU ", "challenging && enjoyable"};
+    char *one[] = {"This ANSI C ", "at ", "is "};
+    char *two[] = {"class", "FIU", "challenging && enjoyable"};
 
     int i;
     for (i = 0; i < 3; ++i) {
-        printf("%s%s", one[i], two[i]);
+        printf("%s%s ", one[i], two[i]);
     }
     printf("\n");
 }
@@ -212,7 +257,6 @@ void printTwo1DArrays()
 int main() 
 {
     int wernerT2DimArray[10][10];
-    int el;
 
     int num1 = generateRandomNumber();
     int num2 = generateRandomNumber();
@@ -220,17 +264,27 @@ int main()
 
     // Part 2 / 2D Array operations
     populateArray(wernerT2DimArray);
-
-    for (int x = 0; x < 10; x++) {
-        printf("Row: %i ------\n", x);
-        for (int y = 0; y < 10; y++) {
-            printf("%i ", wernerT2DimArray[x][y]);
+    printf("Results of poplating 2D array...\n");
+    int x;
+    for (x = 0; x < 10; x++) {
+        // TESTING: printf("Row: %d ------\n", x);
+        int y;
+        for (y = 0; y < 10; y++) {
+            printf("%3d ", wernerT2DimArray[x][y]);
         }
         printf("\n");
     }
+    // Printing divider separating 2D array results
+    int z; 
+    for (z = 0; z < 40; ++z) {
+        printf("-");
+    }
+    printf("\n");
 
     printColumnTotals(wernerT2DimArray);
-    printDifferences(wernerT2DimArray);
+    // Instructions were ambiguous, printing both specific rows - cols
+    // as well as total rows - total columns
+    printTotalDifferences(wernerT2DimArray);
     // Part 3 / sort 3 values
     ascending(num1, num2, num3);
     // Part 4 / print upper case alphabet in reverse
