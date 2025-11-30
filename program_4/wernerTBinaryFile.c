cf247994d9015f17ca9bcb61757358c978d20221
Trevor <trezrevner@icloud.com>
2025-11-29 20:17:49 -0500

    fixed label issue along with logic issue in loops

--- program_4/wernerTBinaryFile.c
+++ program_4/wernerTBinaryFile.c
@@ -1,34 +1,43 @@
-/***********************************************************
-    Author    : Your Name
-    Course    : COP 2270 Sec ANSI C Online
-    Professor : Michael Robinson
-    Program # : 4   
-    Purpose   : Description
-    - Binary File created for use within program 4
-    Due Date  : MM/DD/YYYY
+/***************************************************************
+Author    : Trevor Werner
+Course    : COP 2270 Sec ANSI C Online
+Professor : Michael Robinson
+Program # : 4   
+Purpose   : Description
+            Create a binary output file named numbers.bin containing
+            formatted lines with name, odd/even numbers forwards and backwards.
+            Display each character to the screen as it is being written.
+Due Date  : 11/29/2025
 
-    Certification:
-        I hereby certify that this work is my own and none 
-        of it is the work of any other person.
+Certification:
+I hereby certify that this work is my own and none of it is the work of any other person.
 
-    ..........{ Trevor Werner }..........
-************************************************************/
+..........{ Trevor Werner }..........
+*********************************************************************/
 #include <stdio.h>
 
 #define MIN 0
 #define MAX 25
-
 /* Write a sequence of integers to binary file and echo as readable text */
 void writeSequence(FILE *fp, const char *label, int start, int end, int step)
 {
     int i;
-    printf("%s ", label);     /* Echo label as text on screen */
-    fwrite("- ", sizeof(char), 14, fp);
+    fwrite("-", sizeof(char), 1, fp);
+    printf("-");
+    fwrite(" ", sizeof(char), 1, fp);
+    printf(" ");
+
+    // Write and print label
+    for (i = 0; label[i] != '\0'; i++)
+    {
+        fwrite(&label[i], sizeof(char), 1, fp);
+        printf("%c", label[i]);
+    }
     // step indicates fwd/bkwd direction
     if (step > 0) {
         for (i = start; i <= end; i += step) {
             /* Write raw binary bytes of each int */
-            fwrite(&i, sizeof(int), 1, fp);
+            fwrite(&i, sizeof(char), 1, fp);
             /* Echo readable integer to screen */
             printf("%d ", i);
         }
@@ -38,6 +47,7 @@ void writeSequence(FILE *fp, const char *label, int start, int end, int step)
             printf("%d ", i);
         } 
     }
+    fwrite("\n", sizeof(char), 1, fp);
     printf("\n");
 }
 
@@ -50,28 +60,33 @@ int main(void)
     int i;
     int len = sizeof(name);
 
-    fwrite("- ", sizeof(char), 14, binaryp);
-    printf("My name is: ");
+    fwrite("-", sizeof(char), 1, binaryp);
+    printf("-");
+    fwrite(" ", sizeof(char), 1, binaryp);
+    printf(" ");
 
-    for (i = 0; i < len; i++)
+    for (i = 0; name[i] != '\0'; i++)
     {
-        fwrite(&name[i], sizeof(char), 14, binaryp);
+        fwrite(&name[i], sizeof(char), 1, binaryp);
         printf("%c", name[i]);
     }
+
+    fwrite("\n", sizeof(char), 1, binaryp);
     printf("\n"); /* end w/ newline */
         
     /* 2–4) Forwards */
-    writeSequence(binaryp, "odd numbers forwards:", 1, 25, 2);
-    writeSequence(binaryp, "even numbers forwards:", 0, 24, 2);
-    writeSequence(binaryp, "all numbers forwards:", 0, 25, 1);
+    writeSequence(binaryp, "odd numbers forwards: ", 1, 25, 2);
+    writeSequence(binaryp, "even numbers forwards: ", 0, 24, 2);
+    writeSequence(binaryp, "all numbers forwards: ", 0, 25, 1);
 
     /* 5) Blank line (text version only) */
+    fwrite("\n", sizeof(char), 1, binaryp);
     printf("\n");
 
     /* 6–8) Backwards */
-    writeSequence(binaryp, "odd numbers backwards:", 25, 1, -2);
-    writeSequence(binaryp, "even numbers backwards:", 24, 0, -2);
-    writeSequence(binaryp, "all numbers backwards:", 25, 0, -1);
+    writeSequence(binaryp, "odd numbers backwards: ", 25, 1, -2);
+    writeSequence(binaryp, "even numbers backwards: ", 24, 0, -2);
+    writeSequence(binaryp, "all numbers backwards: ", 25, 0, -1);
 
     fclose(binaryp);
     return 0;
--- program_4/wernerTpgm4.c
+++ program_4/wernerTpgm4.c
@@ -1,19 +1,19 @@
-/*************************************************
-Author   : Trevor Werner
-Course   : COP 2270-U01 (85009) Sec C Prog for Engineers MoWe 12:30 - 1:45
-Professor: Michael Robinson
-Program  : 4
-Purpose:  Purpose of this program is to practice the following concepts:
-    - Read an input text file, copying character by character
-    - Write each character into a new text file
-    - Display each character to the screen, as it is being processed
+/***************************************************************
+Author    : Trevor Werner
+Course    : COP 2270 Sec ANSI C Online
+Professor : Michael Robinson
+Program # : 4   
+Purpose   : Description
+            Read an input text file, copying character by character.
+            Write each character into a new text file.
+            Display each character to the screen, as it is being processed.
+Due Date  : 11/29/2025
 
-Due Date : ????
+Certification:
+I hereby certify that this work is my own and none of it is the work of any other person.
 
-I certify that this work is my own alone.
-
-    ..........{ Trevor Werner }..........
-**************************************************/
+..........{ Trevor Werner }..........
+*********************************************************************/
 #include <stdio.h>
 
 int main(void) 
@@ -24,18 +24,26 @@ int main(void)
     char ch;
     int status;
 
+    // Input file read
     inpFile = fopen(in_name, "r");
+    // Output file written to
     outFile = fopen(out_name, "w");
 
+    // fscanf for accessing file
     for (status = fscanf(inpFile, "%c", &ch); 
-         status != EOF; status = fscanf(inpFile, "%c", &ch)) {
+         status != EOF; status = fscanf(inpFile, "%c", &ch)) 
+    {
+        // print to screen char by char
         printf("%c", ch);
+        // write to file char by char
         fprintf(outFile, "%c", ch); 
     }
 
+    // Close input and output files
     fclose(inpFile);
     fclose(outFile);
 
+    // Status completion notification
     printf("Copied %s to %s.\n", in_name, out_name); 
     return 0;
 }
